ffc1e9572b157784691eb6cfd2ba8d41099c7a62
Oleksii Patsiuk <patsyuck@ukr.net>
2021-07-29 11:41:44 +0300

    Відокремив reducer від store

--- src/redux/authorization/authorizationSelectors.js
+++ src/redux/authorization/authorizationSelectors.js
@@ -1,6 +1,2 @@
-/*export const getToken = state => state.reducer.token
-export const getName = state => state.reducer.user.name*/
-
-/*export const getToken = state => state.persistedReducer.token*/
 export const getAuthorization = state => state.persistedReducer.isLogin
 export const getName = state => (state.persistedReducer.user ? state.persistedReducer.user.name : '')
--- src/redux/contacts/contactsSelectors.js
+++ src/redux/contacts/contactsSelectors.js
@@ -1,7 +1,3 @@
-/*export const getFriends = state => state.reducer.contacts
-export const getFilter = state => state.reducer.filter
-export const getFetching = state => state.reducer.isFetching*/
-
 export const getFriends = state => state.persistedReducer.contacts
 export const getFilter = state => state.persistedReducer.filter
 export const getFetching = state => state.persistedReducer.isFetching
